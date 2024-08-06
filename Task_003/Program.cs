2b406af4f5339d6e3cb29907e18e0760591b3511
Artem_Agashin <agashin.av@mail.ru>
2024-08-06 18:27:58 +0300

    Добавил решение задачи 3

--- Task_003/Program.cs
+++ Task_003/Program.cs
@@ -4,16 +4,26 @@
 
 void Main()
 {
-    int [] array = {1, 2, 3, 4, 5};
+    int [] array = new int [10];
+    Random rand = new Random();
+    System.Console.WriteLine("Исходный произвольный массив: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = rand.Next(0,100);
+        Console.Write (array[i] + "\t");
+    }
+    System.Console.WriteLine();
+    System.Console.WriteLine("Перевернутый массив: ");
     PrintArray(array);
 }
 
 void PrintArray (int [] arr)
 {
     if (arr.Length < 1) return;
-    System.Console.Write(arr[arr.Length - 1] + " ");
-    arr = arr[0..(arr.Length-1)];
+    System.Console.Write(arr[arr.Length - 1] + "\t");
+    arr = arr[..(arr.Length-1)];
     PrintArray(arr);
 }
+
 
 Main();
